156f64dee579a5e8cf5e9c1cc62eb55e6d91736d
Kitze <kristijan.mkd@gmail.com>
2015-03-04 08:39:55 +0100

    feature(exec): make exec global feature, the hash just uses it

--- tasks/hash.js
+++ tasks/hash.js
@@ -1,9 +1,4 @@
 module.exports = function () {
-  var exec = require('child_process').exec;
-  exec("./minify.sh", function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null)
-      console.log('exec error: ' + error);
-  });
+  var exec = require('../functions/exec');
+  exec("./minify.sh");
 };
