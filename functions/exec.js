5ad23782568ad037e4606f0ed36570f60441711d
Kitze <kristijan.mkd@gmail.com>
2015-03-23 15:28:18 +0100

    remove(exec, hash, delete-folder-recursive): not used anymore

--- functions/exec.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (scriptPath) {
-  var exec = require('child_process').exec;
-  exec(scriptPath, function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null)
-      console.log('exec error: ' + error);
-  });
-};
--- tasks/hash.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (gulp, plugins, config) {
-  var exec = require('../functions/exec');
-  exec(config.dirs.prefix + "minify.sh");
-};
