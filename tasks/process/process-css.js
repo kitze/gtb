6eb29b48cbb3b63a882e5b2a5aec7beaef95c3cb
Kristjian Ristovski <kristijan.mkd@gmail.com>
2015-08-19 23:23:57 +0200

    fix(processes): refactor joining of multiple streams with streamqueue instead of gulp-add-src for preserving file order

--- tasks/process/process-css.js
+++ tasks/process/process-css.js
@@ -7,25 +7,30 @@ module.exports = function (gulp, plugins, config) {
   var con = require('../../functions/console');
   var chalk = require('chalk');
   var handleError = require('../../functions/handle-error');
+  var streamqueue = require('streamqueue');
 
   return function () {
     con.hint('Processing css ...');
-    return gulp.src(dir(config.dirs.css + "/application.scss")) //add application.scss (where all the other scss files are included) to the event stream
+
+    var sassStream = gulp.src(dir(config.dirs.css + "/application.scss")) //add application.scss (where all the other scss files are included) to the event stream
       .pipe(plugins.plumber()) // prevents breaking the watcher on an error, just print it out in the console
       .pipe(plugins.cssGlobbing({ //replace global imports (@import some-folder/*) with multiple import lines that node-sass can understand
         extensions: ['.css', '.scss']
       }))
-      .pipe(plugins.sass({onError: handleError, includePaths: [global.prefix + config.dirs.bower + "/"]}))//compile sass files into css (include paths is an array of directories where node-sass can look for files when @import-ing them)
-      .pipe(plugins.addSrc(fileDir("css", "css"))) // add .css files from the project css directory to the event stream
-      //.pipe(plugins.addSrc(getAdditionalLibraries(config.gulp.additionalBowerFiles, "css"))) //get additional bower files defined in the gulp-config and add them to the stream
+      .pipe(plugins.sass({onError: handleError, includePaths: [global.prefix + config.dirs.bower + "/"]})); //compile sass files into css (include paths is an array of directories where node-sass can look for files when @import-ing them)
+
+    var cssStream = gulp.src(fileDir("css", "css"));
+
+    /* Merge sass/css streams before proceeding with task */
+    return streamqueue({objectMode: true}, sassStream, cssStream)
       .pipe(plugins.concat('app.css'))// concatenate everything into app.css file
       .pipe(plugins.autoprefixer({browsers: ['last 2 version']})) // autoprefix the needed css properties so the css is supported on the last 2 versions of every browser
       .pipe(plugins.if(!global.isProduction, plugins.cssbeautify())) //if not in production mode beautify the code so we can easily read the source
       .pipe(plugins.if(global.isProduction, plugins.minifyCss({keepSpecialComments: '*'}))) // if in production mode minify/uglify the css
       .pipe(plugins.if(global.isProduction, plugins.rev()))
       .pipe(gulp.dest(bdir(config.dirs.css))) // place the app.css files into the build/css folder of the project
-      .pipe(plugins.if(global.isProduction, plugins.rev.manifest()) )
-      .pipe(plugins.if(global.isProduction, gulp.dest( bdir('rev/appcss') )))
+      .pipe(plugins.if(global.isProduction, plugins.rev.manifest()))
+      .pipe(plugins.if(global.isProduction, gulp.dest(bdir('rev/appcss'))))
       .pipe(plugins.connect.reload()); //reload the browser after changes
   }
 };
--- tasks/process/process-html.js
+++ tasks/process/process-html.js
@@ -5,6 +5,7 @@ module.exports = function (gulp, plugins, config) {
   var fileDir = require('../../functions/file-dir')(config);
   var con = require('../../functions/console');
   var handleError = require('../../functions/handle-error');
+  var streamqueue = require('streamqueue');
 
   var minifyHtmlOptions = {
     comments: false,
@@ -16,12 +17,17 @@ module.exports = function (gulp, plugins, config) {
 
   return function () {
     con.hint('Processing html ...');
-    gulp.src(fileDir('jade', '')) // get .jade files from the root folder & templates folder
+
+    var jadeStream = gulp.src(fileDir('jade', '')) // get .jade files from the root folder & templates folder
       .pipe(plugins.plumber({errorHandler: handleError})) // prevents breaking the watcher on an error, just print it out in the console
       .pipe(plugins.jade({ // compile jade files to html
         pretty: true
-      }))
-      .pipe(plugins.addSrc(fileDir('html', ''))) // get .html files from root & templates folder
+      }));
+
+    var htmlStream = gulp.src(fileDir('html', ''));
+
+    /* Merge jade/html streams before proceeding with the task */
+    return streamqueue({objectMode: true}, jadeStream, htmlStream)
       .pipe(plugins.if(global.isProduction, plugins.minifyHtml(minifyHtmlOptions))) // if running task in production mode minify html
       .pipe(gulp.dest(bdir(config.dirs.root))) // place the processed .html files accordingly in the folders they belong to
       .pipe(plugins.connect.reload()); // refresh the browser
