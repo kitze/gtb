ebe0f4665b1d4cc2bbba1b815772be44802a69a7
Kristjian Ristovski <kristijan.mkd@gmail.com>
2015-09-01 13:02:38 +0200

    refactor(gulp-config-files): small refactor, use jsonfile, beautify json when writing

--- functions/write-gulp-config-files.js
+++ functions/write-gulp-config-files.js
@@ -1,23 +1,25 @@
+var fs                 = require('fs'),
+    jsonFile           = require('jsonfile'),
+    _                  = require('underscore'),
+    writeFile          = jsonFile.writeFileSync,
+    fileExists         = fs.existsSync,
+    files              = require('../config/files-config'),
+    gulpConfigTemplate = require('../config/gulp-template-config');
+
 module.exports = function () {
-
-  var fs       = require('fs'),
-      jsonfile = require('jsonfile'),
-      files = require('../config/files-config');
-
-  /* If a gulp config doesn't exist for the project generate one that will be global for the project */
-  if (!fs.existsSync(files.GULP_CONFIG)) {
-    fs.writeFileSync(files.GULP_CONFIG, JSON.stringify(gulpConfigTemplate));
+  // If a gulp config doesn't exist for the project generate one that will be global for the project
+  if (!fileExists(files.GULP_CONFIG)) {
+    writeFile(files.GULP_CONFIG, gulpConfigTemplate, {spaces: 2});
   }
 
-  /* If a custom gulp config doesn't exist generate one so every user can have his custom settings */
-  if (!fs.existsSync(files.CUSTOM_CONFIG)) {
-    fs.writeFileSync(files.CUSTOM_CONFIG, JSON.stringify(_(gulpConfigTemplate).omit(
+  // If a custom gulp config doesn't exist generate one so every user can have custom settings locally
+  if (!fileExists(files.CUSTOM_CONFIG)) {
+    writeFile(files.CUSTOM_CONFIG, _(gulpConfigTemplate).omit(
       [
-        'additionalBowerFiles',
         'imagesFolder',
         'copyToFolder'
       ]
-    )));
+    ), {spaces: 2});
   }
 
 };
