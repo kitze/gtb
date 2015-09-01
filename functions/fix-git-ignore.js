f1ea5ba4cace1da6feea419fc2ea8afca27d7fd5
Kristjian Ristovski <kristijan.mkd@gmail.com>
2015-09-01 12:26:55 +0200

    fix(git-ignore): refactor and simplifying logic

--- functions/fix-git-ignore.js
+++ functions/fix-git-ignore.js
@@ -1,30 +1,31 @@
+var fs                    = require('fs'),
+    os                    = require('os'),
+    con                   = require('../functions/console'),
+    _                     = require('underscore'),
+    fileExists            = fs.existsSync,
+    defaultGitIgnoreItems = require('../config/git-ignore-config'),
+    files                 = require('../config/files-config');
+
 module.exports = function () {
 
-  var fs              = require('fs'),
-      os              = require('os'),
-      con             = require('../functions/console'),
-      _               = require('underscore'),
-      shouldGitIgnore = require('../config/git-ignore-config'),
-      files           = require('../config/files-config');
-
-  var newGitIgnore;
-
-  if (fs.existsSync(files.GIT_IGNORE)) {
-    var currentGitIgnoreItems = fs.readFileSync(files.GIT_IGNORE, 'utf8').split(os.EOL);
-    if (currentGitIgnoreItems.length !== 0) {
-      con.hint('Improving .gitignore');
-      _(shouldGitIgnore).each(function (item) {
-        if (currentGitIgnoreItems.indexOf(item) == -1) {
-          currentGitIgnoreItems.push(item);
-        }
-      });
-      newGitIgnore = currentGitIgnoreItems;
-    }
+  function writeGitIgnore(items) {
+    fs.writeFileSync(files.GIT_IGNORE, _(items).compact().join(os.EOL), 'utf8');
   }
-  else {
+
+  // if .gitignore doesn't exist for the project create it and fill it with predefined template
+  if (!fileExists(files.GIT_IGNORE)) {
     con.hint('Creating .gitignore');
-    newGitIgnore = shouldGitIgnore;
+    writeGitIgnore(defaultGitIgnoreItems);
+    return;
   }
 
-  fs.writeFileSync(files.GIT_IGNORE, _(newGitIgnore).compact().join(os.EOL));
+  // get current git ignore file and split it by empty line into an array
+  var existingGitIgnoreItems = fs.readFileSync(files.GIT_IGNORE, 'utf8').split(os.EOL);
+
+  // check if the current .gitignore contains all the content from the .gitignore template, and add it if it doesn't
+  if (existingGitIgnoreItems.length !== 0) {
+    con.hint('Improving .gitignore');
+    writeGitIgnore(_.uniq(existingGitIgnoreItems.concat(defaultGitIgnoreItems)));
+  }
+
 };
