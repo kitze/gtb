92972195c2d5dcf4363719e2ef9c33a52ee8f07a
Kristjian Ristovski <kristijan.mkd@gmail.com>
2015-11-15 21:38:39 +0100

    fix(all-gulp-tasks): improved way of loading tasks

--- tasks/all-gulp-tasks.js
+++ tasks/all-gulp-tasks.js
@@ -1,50 +1,54 @@
-var gulp                 = require('gulp'),
-    _                    = require('lodash'),
-    con                  = require('../functions/console'),
-    files                = require('../config/files-config'),
-    plugins              = require('gulp-load-plugins')({config: files.PACKAGE_JSON}),
-    directories          = require('../config/directories-config');
+var gulp = require('gulp'),
+    _    = require('lodash');
 
-module.exports = function () {
+module.exports = {
 
-  /* Each of the gulp tasks that are in a separate file needs access to "gulp" and "plugins",
-   * so when a task is required
-   * those 3 variables are supplied as arguments
-   * */
+  load: function () {
 
-  function addTask(taskName, taskPath) {
-    gulp.task(taskName, require(files.GULP_TASKS + (taskPath ? taskPath : taskName))(gulp, plugins));
+    var directories = require('../config/directories-config');
+    var files = require('../config/files-config');
+    var plugins = require('gulp-load-plugins')({config: files.PACKAGE_JSON});
+
+    /* Each of the gulp tasks that are in a separate file needs access to "gulp" and "plugins",
+     * so when a task is required
+     * those 3 variables are supplied as arguments
+     * */
+
+    function addTask(taskName, taskPath) {
+      gulp.task(taskName, require(files.GULP_TASKS + (taskPath ? taskPath : taskName))(gulp, plugins));
+    }
+
+    function addTaskFolder(folder, tasksArray) {
+      _.each(tasksArray, function (taskName) {
+        addTask(folder + ':' + taskName, folder + "/" + folder + "-" + taskName);
+      });
+    }
+
+    /*  ================== Add tasks from folders ================== */
+
+    addTaskFolder('process', ['html', 'css', 'js', 'bower', 'fonts', 'images']);
+    addTaskFolder('clean', ['build', 'rev']);
+    addTaskFolder('build', ['only', 'serve']);
+    addTaskFolder('copy', ['build', 'other']);
+    addTaskFolder('deploy', ['surge']);
+
+    /*  ================== Tasks ================== */
+
+    // Run server that will serve index.html and the assets
+    addTask('server');
+
+    // Watch the directories for changes and reload the page, or if a scss/css file is changed inject it automatically without refreshing
+    addTask('watch');
+
+    // Replaces filenames in index.html with the new ones that the rev task produced
+    addTask('replace-rev');
+
+    // Processes bower files, js, css, fonts, images, html
+    addTask('process');
+
+    // Builds the app and runs the server
+    addTask('default');
+
   }
-
-  function addTaskFolder(folder, tasksArray) {
-    _.each(tasksArray, function (taskName) {
-      addTask(folder + ':' + taskName, folder + "/" + folder + "-" + taskName);
-    });
-  }
-
-  /*  ================== Add tasks from folders ================== */
-
-  addTaskFolder('process', ['html', 'css', 'js', 'bower', 'fonts', 'images']);
-  addTaskFolder('clean', ['build', 'rev']);
-  addTaskFolder('build', ['only', 'serve']);
-  addTaskFolder('copy', ['build', 'other']);
-  addTaskFolder('deploy', ['surge']);
-
-  /*  ================== Tasks ================== */
-
-  // Run server that will serve index.html and the assets
-  addTask('server');
-
-  // Watch the directories for changes and reload the page, or if a scss/css file is changed inject it automatically without refreshing
-  addTask('watch');
-
-  // Replaces filenames in index.html with the new ones that the rev task produced
-  addTask('replace-rev');
-
-  // Processes bower files, js, css, fonts, images, html
-  addTask('process');
-
-  // Builds the app and runs the server
-  addTask('default');
 
 };
