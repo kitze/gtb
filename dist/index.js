09bafe7c7411de2809459404a3612581b25d1aa9
Kristjian Ristovski <kristijan.mkd@gmail.com>
2015-11-15 21:39:32 +0100

    feature(app): release new compiled index.js

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,11 @@
 #! /usr/bin/env node
-
-
-//node
 'use strict';
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 var _fs = require('fs');
 
 var _path = require('path');
 
 var _path2 = _interopRequireDefault(_path);
-
-//node_modules
 
 var _lodash = require('lodash');
 
@@ -22,9 +15,9 @@ var _runSequence = require('run-sequence');
 
 var _runSequence2 = _interopRequireDefault(_runSequence);
 
-var _shelljsGlobal = require('shelljs/global');
+var _global = require('shelljs/global');
 
-var _shelljsGlobal2 = _interopRequireDefault(_shelljsGlobal);
+var _global2 = _interopRequireDefault(_global);
 
 var _inquirer = require('inquirer');
 
@@ -46,40 +39,51 @@ var _jsonfile = require('jsonfile');
 
 var _jsonschema = require('jsonschema');
 
-//custom
+var _console = require('../functions/console');
 
-var _functionsConsole = require('../functions/console');
+var _console2 = _interopRequireDefault(_console);
 
-var _functionsConsole2 = _interopRequireDefault(_functionsConsole);
+var _writeGulpConfigFiles = require('../functions/write-gulp-config-files');
 
-var _functionsWriteGulpConfigFiles = require('../functions/write-gulp-config-files');
+var _writeGulpConfigFiles2 = _interopRequireDefault(_writeGulpConfigFiles);
 
-var _functionsWriteGulpConfigFiles2 = _interopRequireDefault(_functionsWriteGulpConfigFiles);
+var _writeBowerConfig = require('../functions/write-bower-config');
 
-var _functionsWriteBowerConfig = require('../functions/write-bower-config');
+var _writeBowerConfig2 = _interopRequireDefault(_writeBowerConfig);
 
-var _functionsWriteBowerConfig2 = _interopRequireDefault(_functionsWriteBowerConfig);
+var _fixGitIgnore = require('../functions/fix-git-ignore');
 
-var _functionsFixGitIgnore = require('../functions/fix-git-ignore');
+var _fixGitIgnore2 = _interopRequireDefault(_fixGitIgnore);
 
-var _functionsFixGitIgnore2 = _interopRequireDefault(_functionsFixGitIgnore);
+var _jsonSchemasConfig = require('../config/json-schemas-config');
 
-var _configJsonSchemasConfig = require('../config/json-schemas-config');
+var _jsonSchemasConfig2 = _interopRequireDefault(_jsonSchemasConfig);
 
-var _configJsonSchemasConfig2 = _interopRequireDefault(_configJsonSchemasConfig);
+var _platformHelpers = require('../functions/platform-helpers');
 
-var _functionsPlatformHelpers = require('../functions/platform-helpers');
+var _platformHelpers2 = _interopRequireDefault(_platformHelpers);
 
-var _functionsPlatformHelpers2 = _interopRequireDefault(_functionsPlatformHelpers);
+var _allGulpTasks = require('../tasks/all-gulp-tasks');
+
+var _allGulpTasks2 = _interopRequireDefault(_allGulpTasks);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 //globals
+
+//node_modules
+
+//node
 global.isProduction = false;
+
+//custom
+
 global.currentProject = undefined;
 
 //constants
 var jsonFileSettings = { spaces: 2 };
 var jsonValidator = new _jsonschema.Validator();
-var operatingSystem = _functionsPlatformHelpers2['default'].getOS();
+var operatingSystem = _platformHelpers2.default.getOS();
 var usersHome = process.env[process.platform == 'win32' ? 'USERPROFILE' : 'HOME'];
 var gtbConfigPath = usersHome + '/.gtb-config.json';
 
@@ -111,22 +115,22 @@ var gtbCommands = [
 
 /* ====== Initialize ====== */
 
-(0, _figlet2['default'])('GTB', function (err, data) {
+(0, _figlet2.default)('GTB', function (err, data) {
   console.log(data.toString());
   gtbConfig = readGtbConfig();
   initialize();
 });
 
 function addCliCommands() {
-  _commander2['default'].option('-n, --name [projectName]', 'option: Run project').option('-t, --task [taskName]', 'option: Run specific gulp task on a project [default]', 'default').option('-p, --production', 'option: Set production mode').option('list', 'command: List all the projects').option('add [add]', 'command: Adds a new project to gtb').option('deploy [deploy]', 'command: Deploys project to surge.sh').option('delete [delete]', 'command: Deletes project').option('.').parse(process.argv);
+  _commander2.default.option('-n, --name [projectName]', 'option: Run project').option('-t, --task [taskName]', 'option: Run specific gulp task on a project [default]', 'default').option('-p, --production', 'option: Set production mode').option('list', 'command: List all the projects').option('add [add]', 'command: Adds a new project to gtb').option('deploy [deploy]', 'command: Deploys project to surge.sh').option('delete [delete]', 'command: Deletes project').option('.').parse(process.argv);
 }
 
 function initialize() {
   addCliCommands();
 
   //if gtb-config doesn't exist, create it then read it and parse the cli arguments
-  if (_lodash2['default'].isUndefined(gtbConfig)) {
-    _functionsConsole2['default'].log('Creating gtb-config for the first time...');
+  if (_lodash2.default.isUndefined(gtbConfig)) {
+    _console2.default.log('Creating gtb-config for the first time...');
     (0, _jsonfile.writeFile)(gtbConfigPath, {
       projects: []
     }, jsonFileSettings, function () {
@@ -141,13 +145,13 @@ function initialize() {
 }
 
 function checkIfCommandExists(name) {
-  var command = _commander2['default'][name];
+  var command = _commander2.default[name];
   return command !== undefined && (typeof command === 'string' || typeof command === 'boolean');
 }
 
 function parseArguments() {
-  var commandWasExecuted = _lodash2['default'].some(gtbCommands, function (command) {
-    return checkIfCommandExists(command.name) && _lodash2['default'].every(command.depends, checkIfCommandExists);
+  var commandWasExecuted = _lodash2.default.some(gtbCommands, function (command) {
+    return checkIfCommandExists(command.name) && _lodash2.default.every(command.depends, checkIfCommandExists);
   });
 
   if (!commandWasExecuted) {
@@ -156,13 +160,13 @@ function parseArguments() {
   }
 
   //lists all the projects
-  if (_commander2['default'].list) {
+  if (_commander2.default.list) {
     listProjects();
     return;
   }
 
-  if (_commander2['default']['delete']) {
-    var foundProject = getProjectByName(_commander2['default']['delete']);
+  if (_commander2.default.delete) {
+    var foundProject = getProjectByName(_commander2.default.delete);
     if (foundProject === undefined) {
       displayProjectNotFoundError();
       return;
@@ -171,8 +175,8 @@ function parseArguments() {
   }
 
   //deploys project
-  if (_commander2['default'].deploy) {
-    var foundProject = getProjectByName(_commander2['default'].deploy);
+  if (_commander2.default.deploy) {
+    var foundProject = getProjectByName(_commander2.default.deploy);
     if (foundProject === undefined) {
       displayProjectNotFoundError();
       return;
@@ -182,10 +186,10 @@ function parseArguments() {
   }
 
   //add a new project
-  if (_commander2['default'].add) {
+  if (_commander2.default.add) {
     var newProject = {};
-    if (typeof _commander2['default'].add === 'string') {
-      if (_commander2['default'].add === '.') {
+    if (typeof _commander2.default.add === 'string') {
+      if (_commander2.default.add === '.') {
         newProject.location = currentProcessPath();
       }
     }
@@ -194,42 +198,42 @@ function parseArguments() {
   }
 
   //sets the production flag
-  if (_commander2['default'].production) {
+  if (_commander2.default.production) {
     global.isProduction = true;
   }
 
-  if (_commander2['default']['.']) {
+  if (_commander2.default['.']) {
     runProject({
       location: currentProcessPath()
-    }, _commander2['default'].task, true);
+    }, _commander2.default.task, true);
     return;
   }
 
-  if (_commander2['default'].name && typeof _commander2['default'].name === 'string') {
-    var foundProject = getProjectByName(_commander2['default'].name);
+  if (_commander2.default.name && typeof _commander2.default.name === 'string') {
+    var foundProject = getProjectByName(_commander2.default.name);
     if (foundProject === undefined) {
       displayProjectNotFoundError();
       return;
     }
 
-    runProject(foundProject, _commander2['default'].task, true);
+    runProject(foundProject, _commander2.default.task, true);
   }
 }
 
 //error messages
 function invalidProjectsJsonMessage() {
-  _functionsConsole2['default'].err('gtb-config is not a valid json file, please check it for errors!');
+  _console2.default.err('gtb-config is not a valid json file, please check it for errors!');
   exit(-1);
 }
 
 function displayProjectNotFoundError() {
-  _functionsConsole2['default'].err('Project with that name wasn\'t found.');
+  _console2.default.err('Project with that name wasn\'t found.');
   displayGtbActions();
 }
 
 function showErrorForProjectsJson(exception) {
-  _functionsConsole2['default'].err('Error in the main gtb-config file:');
-  _functionsConsole2['default'].log(exception.toString());
+  _console2.default.err('Error in the main gtb-config file:');
+  _console2.default.log(exception.toString());
 }
 
 function currentProcessPath() {
@@ -240,7 +244,7 @@ function readGtbConfig() {
 
   //check if file exists
   if (!(0, _fs.existsSync)(gtbConfigPath)) {
-    _functionsConsole2['default'].err('gtb-config file doesn\'t exist.');
+    _console2.default.err('gtb-config file doesn\'t exist.');
     return undefined;
   }
 
@@ -253,13 +257,13 @@ function readGtbConfig() {
   }
 
   //check if the json file follows the correct json schema
-  jsonValidator.addSchema(_configJsonSchemasConfig2['default'].singleProject, '/SingleProjectSchema');
-  var gtbConfigValidation = jsonValidator.validate(gtbConfig, _configJsonSchemasConfig2['default'].gtbConfig);
+  jsonValidator.addSchema(_jsonSchemasConfig2.default.singleProject, '/SingleProjectSchema');
+  var gtbConfigValidation = jsonValidator.validate(gtbConfig, _jsonSchemasConfig2.default.gtbConfig);
 
   if (gtbConfigValidation.errors.length > 0) {
-    _functionsConsole2['default'].err('Errors in gtb-config file:');
-    _lodash2['default'].each(gtbConfigValidation.errors, function (error) {
-      _functionsConsole2['default'].log(error.stack);
+    _console2.default.err('Errors in gtb-config file:');
+    _lodash2.default.each(gtbConfigValidation.errors, function (error) {
+      _console2.default.log(error.stack);
     });
     exit(-1);
   }
@@ -268,20 +272,20 @@ function readGtbConfig() {
 }
 
 function getProjectByName(name) {
-  return _lodash2['default'].findWhere(gtbConfig.projects, { name: name });
+  return _lodash2.default.findWhere(gtbConfig.projects, { name: name });
 }
 
 function checkNumberOfProjects() {
   var projects = gtbConfig.projects;
   if (projects.length === 0) {
-    _functionsConsole2['default'].hint('You don\'t have any projects in gtb yet, so let\'s add your first one!');
+    _console2.default.hint('You don\'t have any projects in gtb yet, so let\'s add your first one!');
     addNewProjectPrompt();
     return 0;
   }
 
   // If there's only run project run that one by default *!/
   if (projects.length === 1) {
-    _functionsConsole2['default'].hint('You only have one project in gtb, choosing that one by default.');
+    _console2.default.hint('You only have one project in gtb, choosing that one by default.');
     performAction(projects[0]);
     return 1;
   }
@@ -298,7 +302,7 @@ function listProjects() {
     return;
   }
 
-  _inquirer2['default'].prompt({
+  _inquirer2.default.prompt({
     type: 'list',
     name: 'pickedProject',
     message: 'Pick a project:',
@@ -309,8 +313,8 @@ function listProjects() {
 }
 
 function deleteProject(project) {
-  _functionsConsole2['default'].hint('The project "' + project.name + '" was successfully deleted!');
-  gtbConfig.projects = _lodash2['default'].filter(gtbConfig.projects, function (p) {
+  _console2.default.hint('The project "' + project.name + '" was successfully deleted!');
+  gtbConfig.projects = _lodash2.default.filter(gtbConfig.projects, function (p) {
     return p.name !== project.name;
   });
   updateGtbConfig(true, gtbConfig);
@@ -326,13 +330,13 @@ function performAction(project) {
   }, {
     name: 'Build production version',
     value: function value() {
-      _functionsConsole2['default'].log('Building production version of project "' + project.name + '"...');
+      _console2.default.log('Building production version of project "' + project.name + '"...');
       runProject(project, 'build:only', false);
     }
   }, {
     name: 'Build and run production version',
     value: function value() {
-      _functionsConsole2['default'].log('Running production version of project "' + project.name + '"...');
+      _console2.default.log('Running production version of project "' + project.name + '"...');
       runProject(project, 'build:serve', false);
     }
   }, {
@@ -347,7 +351,7 @@ function performAction(project) {
     }
   }];
 
-  _inquirer2['default'].prompt({
+  _inquirer2.default.prompt({
     type: 'list',
     name: 'actionToRun',
     message: 'Pick which action would you like to perform on the project "' + project.name + '"?',
@@ -380,7 +384,7 @@ function displayGtbActions(project) {
     choices: actions
   }];
 
-  _inquirer2['default'].prompt(prompt, function (answers) {
+  _inquirer2.default.prompt(prompt, function (answers) {
     answers.nextAction();
   });
 }
@@ -394,7 +398,7 @@ function updateGtbConfig(displayListOfProjects, newConfig) {
   var config = newConfig === undefined ? gtbConfig : newConfig;
   (0, _jsonfile.writeFile)(gtbConfigPath, config, jsonFileSettings, function (error) {
     if (error) {
-      _functionsConsole2['default'].error(error);
+      _console2.default.error(error);
       exit(-1);
     }
 
@@ -409,24 +413,24 @@ function validateNewProjectName(name) {
 
   //name validation
   if (name === '') {
-    _functionsConsole2['default'].errorWithSpaces('Please provide a name for the project!');
+    _console2.default.errorWithSpaces('Please provide a name for the project!');
     return false;
   }
 
   if (name.indexOf(" ") !== -1) {
-    _functionsConsole2['default'].errorWithSpaces('The project name cannot contain spaces!');
+    _console2.default.errorWithSpaces('The project name cannot contain spaces!');
     return false;
   }
 
   if (name.length > 20) {
-    _functionsConsole2['default'].errorWithSpaces('The project name cannot be longer than 20 chars!');
+    _console2.default.errorWithSpaces('The project name cannot be longer than 20 chars!');
     return false;
   }
 
-  var projectWithSameName = _lodash2['default'].findWhere(gtbConfig.projects, { name: name });
+  var projectWithSameName = _lodash2.default.findWhere(gtbConfig.projects, { name: name });
 
-  if (!_lodash2['default'].isUndefined(projectWithSameName)) {
-    _functionsConsole2['default'].errorWithSpaces('Project with that name already exists, please choose another name!');
+  if (!_lodash2.default.isUndefined(projectWithSameName)) {
+    _console2.default.errorWithSpaces('Project with that name already exists, please choose another name!');
     return false;
   }
 
@@ -439,21 +443,21 @@ function validateNewProjectPath(path) {
 
   //location validation
   if (path.indexOf(" ") !== -1) {
-    _functionsConsole2['default'].errorWithSpaces('The path of the project cannot contain spaces!');
+    _console2.default.errorWithSpaces('The path of the project cannot contain spaces!');
     return false;
   }
 
   if (path === '') {
-    _functionsConsole2['default'].errorWithSpaces('Please provide a path for the project!');
+    _console2.default.errorWithSpaces('Please provide a path for the project!');
     return false;
   }
 
-  var projectWithSamePath = _lodash2['default'].find(gtbConfig.projects, function (project) {
+  var projectWithSamePath = _lodash2.default.find(gtbConfig.projects, function (project) {
     return project.location === path || project.location === path + '/';
   });
 
-  if (!_lodash2['default'].isUndefined(projectWithSamePath)) {
-    _functionsConsole2['default'].errorWithSpaces('Project with that path already exists, please choose another path!');
+  if (!_lodash2.default.isUndefined(projectWithSamePath)) {
+    _console2.default.errorWithSpaces('Project with that path already exists, please choose another path!');
     return false;
   }
 
@@ -461,7 +465,7 @@ function validateNewProjectPath(path) {
   path = fixPathTrailingSlash(path);
 
   if (!(0, _fs.existsSync)(path)) {
-    _functionsConsole2['default'].errorWithSpaces('The path of the project is not valid, please add a valid path!');
+    _console2.default.errorWithSpaces('The path of the project is not valid, please add a valid path!');
     return false;
   }
 
@@ -477,7 +481,7 @@ function fixPathTrailingSlash(path) {
 }
 
 function addNewProjectPrompt(existingProject) {
-  var pathExample = _functionsPlatformHelpers2['default'].getExamplePathByOs();
+  var pathExample = _platformHelpers2.default.getExamplePathByOs();
 
   var nameQuestion = {
     type: 'input',
@@ -506,8 +510,8 @@ function addNewProjectPrompt(existingProject) {
     questions.push(pathQuestion);
   }
 
-  _inquirer2['default'].prompt(questions, function (newProject) {
-    saveNewProject(existingProject !== undefined ? _lodash2['default'].extend(existingProject, newProject) : newProject);
+  _inquirer2.default.prompt(questions, function (newProject) {
+    saveNewProject(existingProject !== undefined ? _lodash2.default.extend(existingProject, newProject) : newProject);
   });
 }
 
@@ -517,20 +521,20 @@ function runProject(project, task, displayLog) {
 
   //log
   if (displayLog && project.name !== undefined) {
-    _functionsConsole2['default'].log('Running project "' + project.name + '"...');
+    _console2.default.log('Running project "' + project.name + '"...');
   }
 
   //write configs and gitignore
-  (0, _functionsWriteGulpConfigFiles2['default'])().then(function () {
-    (0, _functionsWriteBowerConfig2['default'])();
+  (0, _writeGulpConfigFiles2.default)().then(function () {
+    (0, _writeBowerConfig2.default)();
 
     //only fix gitignore if the user hasn't specified a task to run
-    if (task === undefined || task === undefined) {
-      (0, _functionsFixGitIgnore2['default'])();
+    if (task === undefined) {
+      (0, _fixGitIgnore2.default)();
     }
 
     //gulp
-    require('../tasks/all-gulp-tasks')();
-    (0, _runSequence2['default'])(task === undefined ? 'default' : task);
+    _allGulpTasks2.default.load();
+    (0, _runSequence2.default)(task === undefined ? 'default' : task);
   });
 }
